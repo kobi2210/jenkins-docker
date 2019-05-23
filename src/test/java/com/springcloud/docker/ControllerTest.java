a12b928c2ea5b39df1dea164702eec0c09c9c795
shuangquandong <shuangquandong@dingtalk.com>
2019-05-23 10:48:13 +0800

    add

--- src/test/java/com/springcloud/docker/ControllerTest.java
+++ src/test/java/com/springcloud/docker/ControllerTest.java
@@ -1,9 +1,13 @@
 package com.springcloud.docker;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -21,12 +25,13 @@ public class ControllerTest extends AppTest {
 
 	@Test
 	public void test() {
-		/*
-		 * System.out.print("============" + mockMvc); try {
-		 * mockMvc.perform(MockMvcRequestBuilders.get("/test")).andExpect(
-		 * MockMvcResultMatchers.status().isOk()) .andDo(MockMvcResultHandlers.print());
-		 * } catch (Exception e) { // TODO Auto-generated catch block Assert.fail();
-		 * e.printStackTrace(); }
-		 */
+		try {
+			mockMvc.perform(MockMvcRequestBuilders.get("/test")).andExpect(MockMvcResultMatchers.status().isOk())
+					.andDo(MockMvcResultHandlers.print());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			Assert.fail();
+			e.printStackTrace();
+		}
 	}
 }
